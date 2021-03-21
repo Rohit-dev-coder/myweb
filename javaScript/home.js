4db3f7c1ebf375285ea550e03c322987d520985d
Rohit-dev-coder <jarariyarohti171198@gmail.com>
2021-03-21 20:26:23 +0530

    forthday1

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,33 +1,33 @@
-let t_bool = false;
-let btn = document.getElementById('tooglebtn');
-btn.onclick = function () {
-    if (t_bool == false) {
-        btn.setAttribute('src', './icons/outline_close_black_24dp.png');
-        t_bool = true;
-    } else {
-        btn.setAttribute('src', './icons/outline_menu_black_24dp.png');
-        t_bool = false;
-    }
-}
-window.onscroll = function () {
-    myFunction();
-};
+// let t_bool = false;
+// let btn = document.getElementById('tooglebtn');
+// btn.onclick = function () {
+//     if (t_bool == false) {
+//         btn.setAttribute('src', './icons/outline_close_black_24dp.png');
+//         t_bool = true;
+//     } else {
+//         btn.setAttribute('src', './icons/outline_menu_black_24dp.png');
+//         t_bool = false;
+//     }
+// }
+// window.onscroll = function () {
+//     myFunction();
+// };
 
-var header = document.getElementById("myconfig0");
-var sticky = header.offsetTop;
+// var header = document.getElementById("myconfig0");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.remove("sticky");
+//     }
+// }
 
 // window.onscroll = function () {
 //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-       
+//         header.classList.add("sticky");
 //     } else {
-        
+//         header.classList.remove("sticky");
 //     }
 // };
